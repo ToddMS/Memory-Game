f3509ce95d91049c873384c488f203913c96c515
T Sandler <toddsandlerwasd@gmail.com>
2021-07-28 13:42:02 +0100

    started memory game kotlin app

---
+++ app/src/main/java/com/toddsandler/memorygame/MemoryBoardAdapter.kt
@@ -0,0 +1,39 @@
+package com.toddsandler.memorygame
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
+
+class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
+    RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val cardWidth = parent.width / 2
+        val cardHeight = parent.height / 4
+        val cardSideLength = min(cardWidth, cardHeight)
+        val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount() = numPieces
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(position: Int) {
+            // No-op
+
+        }
+    }
+}
